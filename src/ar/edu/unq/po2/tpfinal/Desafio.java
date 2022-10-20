c63f6bc886344cccb4e8c70cac96087b1ac89c00
ormendez <ormendez@BAI-54DF5M3.usersad.everis.int>
2022-10-19 23:05:52 -0300

    elementoDeLudificacion + desafio

--- src/ar/edu/unq/po2/tpfinal/Desafio.java
+++ src/ar/edu/unq/po2/tpfinal/Desafio.java
@@ -1,5 +1,15 @@
 package ar.edu.unq.po2.tpfinal;
 
-public class Desafio {
+import java.awt.geom.Area;
 
+public class Desafio extends ElementoDeLudificacion {
+
+	private Area area; 
+	// private restricciones (ver)
+	private int cantidadDeMuestras; 
+	private int dificultad; 
+	private int recompensa; 
+	// private boolean estaCompleta; (ver)
+	
+	
 }
