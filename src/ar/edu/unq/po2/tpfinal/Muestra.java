6637e8188efb1fdf7b4654171f01836968211e49
bragonip <pablo.bragoni@gmail.com>
2022-10-19 23:52:55 -0300

    Completé la clase Muestra

--- src/ar/edu/unq/po2/tpfinal/Muestra.java
+++ src/ar/edu/unq/po2/tpfinal/Muestra.java
@@ -5,13 +5,52 @@ import java.time.LocalTime;
 
 public class Muestra {
 
-	private Coordenada coordenadas; 
+	private Coordenada coordenada; 
 	private Usuario usuario; 
 	private LocalDate fecha; 
 	private LocalTime hora;
 	
-	public Muestra() {
-		
+	public Muestra(Coordenada coordenada, Usuario usuario, LocalDate fecha,LocalTime hora) {
+		this.setCoordenada(coordenada);
+		this.setUsuario(usuario);
+		this.setFecha(fecha);
+		this.setHora(hora);
+	}
+
+	//borrar si no lo terminamos usando
+	private Coordenada getCoordenada() {
+		return this.coordenada;
+	}
+
+	private void setCoordenada(Coordenada coordenada) {
+		this.coordenada = coordenada;
+	}
+
+	//borrar si no lo terminamos usando
+	private Usuario getUsuario() {
+		return this.usuario;
+	}
+
+	private void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	//borrar si no lo terminamos usando
+	private LocalDate getFecha() {
+		return this.fecha;
+	}
+
+	private void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+
+	//borrar si no lo terminamos usando
+	private LocalTime getHora() {
+		return this.hora;
+	}
+
+	private void setHora(LocalTime hora) {
+		this.hora = hora;
 	}
 	
 }
