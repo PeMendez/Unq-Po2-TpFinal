7654a07d03e3e828d93c4065d799abb77ac4f1d4
bragonip <pablo.bragoni@gmail.com>
2022-10-23 15:05:43 -0300

    Termine la restriccion temporal

--- src/ar/edu/unq/po2/tpfinal/RestriccionTemporal.java
+++ src/ar/edu/unq/po2/tpfinal/RestriccionTemporal.java
@@ -1,72 +1,65 @@
 package ar.edu.unq.po2.tpfinal;
 
-import java.time.LocalDate; 
+import java.time.LocalDate;
 
 public class RestriccionTemporal {
-	
-	private String tipo;
-	private LocalDate inicio;
-	private LocalDate fin;
-	
-	
+
+	private String tipo = null;
+	private LocalDate inicio = null;
+	private LocalDate fin = null;
+
 	public RestriccionTemporal(String tipo) {
 		this.setTipo(tipo);
-		this.setInicio(LocalDate.of(1900,01,01)); // una fecha ridiculamente vieja
-		this.setFin(LocalDate.of(3000,01,01)); // una fecha ridiculamente futura
-		
-	}
-	public RestriccionTemporal(LocalDate inicio, LocalDate fin) {
-		this.setTipo("algo que incluya a ambas");
-		this.setInicio(inicio); // una fecha ridiculamente vieja
-		this.setFin(fin); // una fecha ridiculamente futura
-		
-	}
-	
-	public RestriccionTemporal(String tipo, LocalDate inicio, LocalDate fin) {
-		this.setTipo(tipo);
-		this.setInicio(inicio); // una fecha ridiculamente vieja
-		this.setFin(fin); // una fecha ridiculamente futura
-		
 	}
 
+	public RestriccionTemporal(LocalDate inicio, LocalDate fin) {
+		this.setInicio(inicio);
+		this.setFin(fin);
+	}
+
+	public RestriccionTemporal(String tipo, LocalDate inicio, LocalDate fin) {
+		this.setTipo(tipo);
+		this.setInicio(inicio);
+		this.setFin(fin);
+	}
 
 	private String getTipo() {
 		return tipo;
 	}
 
-
 	private void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
-
 
 	private LocalDate getInicio() {
 		return inicio;
 	}
 
-
 	private void setInicio(LocalDate inicio) {
 		this.inicio = inicio;
 	}
-
 
 	private LocalDate getFin() {
 		return fin;
 	}
 
-
 	private void setFin(LocalDate fin) {
 		this.fin = fin;
 	}
 
-//	public boolean estaHabilitado(Date fecha) {
-//		switch (this.tipo) {
-//		case "diasHabiles":
-//			return fecha.esDiaHabil() && fecha.after(inicio) && fecha.before(fin);
-//			break;
-//		case "finDeSemana":
-//			return fecha.esFinDeSemana() && fecha.after(inicio) && fecha.before(fin);
-//			break;
-//		}
-//	}
+	public boolean estaHabilitado(LocalDate fecha) {
+		switch (this.getTipo()) {
+		case "diasHabiles":
+			return fecha.getDayOfWeek().getValue() < 6 && this.estaDentroDelPeriodo(fecha);
+		case "finDeSemana":
+			return fecha.getDayOfWeek().getValue() > 5 && this.estaDentroDelPeriodo(fecha);
+		default:
+			return this.estaDentroDelPeriodo(fecha);
+		}
+	}
+
+	private boolean estaDentroDelPeriodo(LocalDate fecha) {
+		return (this.getInicio() == null && this.getFin() == null)
+				|| (fecha.isAfter(this.getInicio()) && fecha.isBefore(this.getFin()));
+	}
 }
