7dfeee3d1e7eed04cedec21e89193aca544faa09
bragonip <pablo.bragoni@gmail.com>
2022-11-01 21:30:58 -0300

    Agregue test para diashabiles y findesemana

---
+++ test/ar/edu/unq/po2/tpfinal/DiasHabilesTest.java
@@ -0,0 +1,34 @@
+package ar.edu.unq.po2.tpfinal;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import ar.edu.unq.po2.tpfinal.RestriccionTemporal.DiasHabiles;
+import ar.edu.unq.po2.tpfinal.RestriccionTemporal.TipoDeRestriccion;
+
+class DiasHabilesTest {
+
+	private TipoDeRestriccion diasHabiles;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		diasHabiles = new DiasHabiles();
+	}
+
+	@Test
+	void testEstaHabilitadoTrue() {
+		assertTrue(diasHabiles.estaHabilitado(LocalDate.of(2022, 10, 17))); // Lunes
+		assertTrue(diasHabiles.estaHabilitado(LocalDate.of(2022, 10, 18))); // Martes
+		assertTrue(diasHabiles.estaHabilitado(LocalDate.of(2022, 10, 19))); // Miércoles
+		assertTrue(diasHabiles.estaHabilitado(LocalDate.of(2022, 10, 20))); // Jueves
+		assertTrue(diasHabiles.estaHabilitado(LocalDate.of(2022, 10, 21))); // Viernes
+	}
+
+	@Test
+	void testEstaHabilitadoFalse() {
+		assertFalse(diasHabiles.estaHabilitado(LocalDate.of(2022, 10, 22))); // Sábado
+		assertFalse(diasHabiles.estaHabilitado(LocalDate.of(2022, 10, 23))); // Domingo
+	}
+}
