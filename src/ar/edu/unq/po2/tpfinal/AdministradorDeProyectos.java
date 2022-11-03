89c51708d5a034b23773c8bb661952430f212992
Walter <walter.laborde@hotmail.com>
2022-11-03 15:56:29 -0300

    clase Administrador De Proyectos

---
+++ src/ar/edu/unq/po2/tpfinal/AdministradorDeProyectos.java
@@ -0,0 +1,17 @@
+package ar.edu.unq.po2.tpfinal;
+
+import java.util.List;
+
+public class AdministradorDeProyectos {
+
+	private List<Proyecto> proyDisponibles;
+
+	public List<Proyecto> getProyDisponibles() {
+		return proyDisponibles;
+	}
+
+	public void setProyDisponibles(List<Proyecto> proyDisponibles) {
+		this.proyDisponibles = proyDisponibles;
+	}
+	
+}
