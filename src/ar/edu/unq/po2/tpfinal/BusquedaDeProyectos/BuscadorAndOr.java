6e674a6fa0966568b3172efddb89ed0b45209730
Walter <walter.laborde@hotmail.com>
2022-11-03 16:25:17 -0300

    composite AndOr

---
+++ src/ar/edu/unq/po2/tpfinal/BusquedaDeProyectos/BuscadorAndOr.java
@@ -0,0 +1,31 @@
+package ar.edu.unq.po2.tpfinal.BusquedaDeProyectos;
+
+import java.util.List;
+
+
+import ar.edu.unq.po2.tpfinal.Proyecto;
+
+public class BuscadorAndOr extends TipoDeBusqueda{
+
+	private List<TipoDeBusqueda> busquedasAndOr;
+	
+	@Override
+	public List<Proyecto> buscarEnProyectos(String loQueBusco) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void agregarTipoDeBusqueda(TipoDeBusqueda nuevoTipo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void borrarTipoDeBusqueda(TipoDeBusqueda nuevoTipo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
