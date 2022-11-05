6f759c42dbaf7f2bf400ffe8fd805fbb9aa8b91b
bragonip <pablo.bragoni@gmail.com>
2022-11-05 19:34:33 -0300

    renombre el test del la restriccion geografica

--- test/ar/edu/unq/po2/tpfinal/RestriccionGeograficaTest.java
+++ test/ar/edu/unq/po2/tpfinal/RestriccionGeograficaTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
-class AreaTest {
+class RestriccionGeograficaTest {
 
 	private RestriccionGeografica restriccion;
 	@Mock
