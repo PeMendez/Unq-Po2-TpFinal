9cfe9698b6581be99f8e0aa7db3f0406fce15d41
Walter <walter.laborde@hotmail.com>
2022-11-05 09:07:09 -0300

    modificacion de nombre de clase de test

--- test/ar/edu/unq/po2/tpfinal/ProyectoTest.java
+++ test/ar/edu/unq/po2/tpfinal/ProyectoTest.java
@@ -20,7 +20,7 @@ import org.mockito.Mock;
 
 //import ar.edu.unq.po2.tpfinal.StateDesafios.*;
 
-class UsuarioTest {
+class ProyectoTest {
 
 	private Proyecto proyect; //sut
 	private String nombre1; 
