8f5bb18aec79167289b39f7d9606d202666d12f6
Walter <walter.laborde@hotmail.com>
2022-11-06 19:41:22 -0300

    test estaCompleto

--- test/ar/edu/unq/po2/tpfinal/EstadoPendienteTest.java
+++ test/ar/edu/unq/po2/tpfinal/EstadoPendienteTest.java
@@ -1,6 +1,8 @@
 package ar.edu.unq.po2.tpfinal;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.*;
 import org.mockito.Mock;
@@ -9,12 +11,18 @@ import ar.edu.unq.po2.tpfinal.StateDesafios.*;
 
 public class EstadoPendienteTest {
 	
+	private EstadoPendiente estadoPend;
+	@Mock private EstadoDesafio estadoEnCurso;
 	@Mock Desafio desafio; 
 	@Mock Usuario usuario; 
 	private DesafioUsuario du; 
+	@Mock private DesafioUsuario du2; 
 	
 	@BeforeEach 
 	public void setUp() {
+		estadoPend = new EstadoPendiente();
+		estadoEnCurso = mock(EstadoDesafio.class);
+		du2 = mock(DesafioUsuario.class);
 		
 		du = new DesafioUsuario(usuario, desafio);
 	}
@@ -29,6 +37,16 @@ public class EstadoPendienteTest {
 		//verify
 		assertFalse(du.getEstado().estaCompleto(du));
 
+	}
+	
+	@Test
+	void testEstaCompleto() {
+		//setup
+	
+		//exercise
+		
+		//verify
+		assertFalse(estadoPend.estaCompleto(du2));
 	}
 	
 	
