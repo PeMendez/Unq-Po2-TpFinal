e03e18357f4d34b1dde48619a571159c19440e9b
Walter <walter.laborde@hotmail.com>
2022-11-06 19:03:15 -0300

    Merge branch 'main' of https://github.com/PeMendez/Unq-Po2-TpFinal.git into main cambio el porcentaje de completitud a 1 en vez de 100

--- src/ar/edu/unq/po2/tpfinal/StateDesafios/EstadoCompleto.java
+++ src/ar/edu/unq/po2/tpfinal/StateDesafios/EstadoCompleto.java
@@ -11,7 +11,7 @@ public class EstadoCompleto extends EstadoDesafio{
 	@Override
 	public float porcentajeDeCompletitud(DesafioUsuario desafio) {
 		
-		return 100;
+		return 1.00f;
 	}
 
 	@Override
