49d6c57c46145f04fc8105d11069a4fcea940fac
bragonip <pablo.bragoni@gmail.com>
2022-11-20 16:43:47 -0300

    arregle FinDeSemanaTest

--- test/ar/edu/unq/po2/tpfinal/FinDeSemanaTest.java
+++ test/ar/edu/unq/po2/tpfinal/FinDeSemanaTest.java
@@ -1,6 +1,8 @@
 package ar.edu.unq.po2.tpfinal;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import java.time.LocalDate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,26 +12,59 @@ import ar.edu.unq.po2.tpfinal.RestriccionTemporal.FinDeSemana;
 class FinDeSemanaTest {
 
 	private FinDeSemana finDeSemana;
+	private Muestra muestra;
 
 	@BeforeEach
 	void setUp() throws Exception {
 		finDeSemana = new FinDeSemana();
+		muestra = mock(Muestra.class);
 	}
 
 	@Test
 	void testEstaHabilitadoTrue() {
+		// sabado
+		// set up
+		when(muestra.getFecha()).thenReturn(LocalDate.of(2022, 10, 22));
 		// verify
-		assertTrue(finDeSemana.seCumple(LocalDate.of(2022, 10, 22))); // Sábado
-		assertTrue(finDeSemana.seCumple(LocalDate.of(2022, 10, 23))); // Domingo
+		assertTrue(finDeSemana.seCumple(muestra));
+
+		// domingo
+		// set up
+		when(muestra.getFecha()).thenReturn(LocalDate.of(2022, 10, 23));
+		// verify
+		assertTrue(finDeSemana.seCumple(muestra));
 	}
 
 	@Test
 	void testEstaHabilitadoFalse() {
+		// lunes
+		// set up
+		when(muestra.getFecha()).thenReturn(LocalDate.of(2022, 10, 17));
 		// verify
-		assertFalse(finDeSemana.seCumple(LocalDate.of(2022, 10, 17))); // Lunes
-		assertFalse(finDeSemana.seCumple(LocalDate.of(2022, 10, 18))); // Martes
-		assertFalse(finDeSemana.seCumple(LocalDate.of(2022, 10, 19))); // Miércoles
-		assertFalse(finDeSemana.seCumple(LocalDate.of(2022, 10, 20))); // Jueves
-		assertFalse(finDeSemana.seCumple(LocalDate.of(2022, 10, 21))); // Viernes
+		assertFalse(finDeSemana.seCumple(muestra));
+		
+		// martes
+		// set up
+		when(muestra.getFecha()).thenReturn(LocalDate.of(2022, 10, 18));
+		// verify
+		assertFalse(finDeSemana.seCumple(muestra));
+		
+		// miercoles
+		// set up
+		when(muestra.getFecha()).thenReturn(LocalDate.of(2022, 10, 19));
+		// verify
+		assertFalse(finDeSemana.seCumple(muestra));
+		
+		// jueves
+		// set up
+		when(muestra.getFecha()).thenReturn(LocalDate.of(2022, 10, 10));
+		// verify
+		assertFalse(finDeSemana.seCumple(muestra));
+		
+		// viernes
+		// set up
+		when(muestra.getFecha()).thenReturn(LocalDate.of(2022, 10, 21));
+		// verify
+		assertFalse(finDeSemana.seCumple(muestra));
 	}
 }
