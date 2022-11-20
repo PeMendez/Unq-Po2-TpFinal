45093399dbbd9614dd77356d7e5f723be2c2dd22
bragonip <pablo.bragoni@gmail.com>
2022-11-20 17:50:21 -0300

    agregue un test a la muestra

--- test/ar/edu/unq/po2/tpfinal/MuestraTest.java
+++ test/ar/edu/unq/po2/tpfinal/MuestraTest.java
@@ -8,16 +8,17 @@ import java.time.LocalTime;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+
 
 class MuestraTest {
 
 	private Muestra muestra;
-	@Mock private Coordenada coordenada; 
-	@Mock private Usuario usuario; 
-	private LocalDate fecha; 
+	private Coordenada coordenada;
+	private Usuario usuario;
+	private LocalDate fecha;
 	private LocalTime hora;
-	
+	private RestriccionGeografica area;
+
 	@BeforeEach
 	public void setup() {
 		fecha = LocalDate.of(2022, 5, 11);
@@ -25,37 +26,44 @@ class MuestraTest {
 		coordenada = mock(Coordenada.class);
 		usuario = mock(Usuario.class);
 		muestra = new Muestra(coordenada, usuario, fecha, hora);
+		area = mock(RestriccionGeografica.class);
 	}
-	
+
 	@Test
 	void testGetCoordenada() {
-		//setup
-		//exercise
-		//verify
-		assertEquals(coordenada,muestra.getCoordenada());
+		// setup
+		// exercise
+		// verify
+		assertEquals(coordenada, muestra.getCoordenada());
 	}
-	
+
 	@Test
 	void testGetUsuario() {
-		//setup
-		//exercise
-		//verify
-		assertEquals(usuario,muestra.getUsuario());
+		// setup
+		// exercise
+		// verify
+		assertEquals(usuario, muestra.getUsuario());
 	}
-	
+
 	@Test
 	void testGetFecha() {
-		//setup
-		//exercise
-		//verify
-		assertEquals(LocalDate.of(2022, 5, 11),muestra.getFecha());
+		// setup
+		// exercise
+		// verify
+		assertEquals(LocalDate.of(2022, 5, 11), muestra.getFecha());
 	}
-	
+
 	@Test
 	void testGetHora() {
-		//setup
-		//exercise
-		//verify
-		assertEquals(LocalTime.of(15,25),muestra.getHora());
+		// setup
+		// exercise
+		// verify
+		assertEquals(LocalTime.of(15, 25), muestra.getHora());
+	}
+
+	@Test
+	void testEsMuestraValidaTrue() {
+		muestra.esMuestraValida(area);
+		verify(area,times(1)).seCumple(muestra);
 	}
 }
