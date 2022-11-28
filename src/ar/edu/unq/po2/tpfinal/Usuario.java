ac92fe6c5b43751caf400a96b1201a20ae4dd1ac
Ornella Mendez <hope.peque@gmail.com>
2022-11-28 20:15:35 -0300

    borro acoplamiento, rompo strategy

--- src/ar/edu/unq/po2/tpfinal/Usuario.java
+++ src/ar/edu/unq/po2/tpfinal/Usuario.java
@@ -9,7 +9,6 @@ import ar.edu.unq.po2.tpfinal.StateDesafios.*;
 public class Usuario {
 
 	private List<Muestra> muestras; 
-	private List<Proyecto> proyectos; 
 	private Perfil perfil; 
 	private List<DesafioUsuario> desafiosUsuarios; 
 	
@@ -17,19 +16,16 @@ public class Usuario {
 		
 		this.setPerfil(perfil);
 		this.setMuestras(new ArrayList<Muestra>()); 
-		this.proyectos = new ArrayList<Proyecto>();
 		this.desafiosUsuarios =  new ArrayList<DesafioUsuario>();
 	}
 	
 	public void suscribirse(Proyecto proyecto) {
-		
-		this.proyectos.add(proyecto); 
+ 
 		proyecto.addParticipante(this);
 	}
 	
 	public void desuscribirse(Proyecto proyecto) {
 		
-		this.getProyectos().remove(proyecto); 
 		proyecto.desuscribirParticipante(this); 
 	}
 	
@@ -69,14 +65,6 @@ public class Usuario {
 
 	public void setPerfil(Perfil perfil) {
 		this.perfil = perfil;
-	}
-	
-	public List<Proyecto> getProyectos() {
-		return this.proyectos;
-	}
-
-	public void setProyectos(List<Proyecto> proyectos) {
-		this.proyectos = proyectos;
 	}
 
 	public List<Muestra> getMuestras() {
